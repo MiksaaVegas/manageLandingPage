af9b867f444a426cefdfd8f8c96a5e31f50f0da6
Mihail <kuzmanoskimihail9@gmail.com>
2022-02-06 15:14:57 +0100

    created and styled the footer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   loop: true,
   autoplay: {
     delay: 2000,
     pauseOnMouseEnter: true,
   },
   spaceBetween: 40,
-  slidesPerView: 3
-});
+  slidesPerView: 3,
+})
+const emailInput = document.querySelector(".emailInput")
+const footerSubmit = document.querySelector(".footerSubmit")
+const errorMsg = document.querySelector(".error")
+footerSubmit.addEventListener("click", function () {
+  let enteredEmail = emailInput.value
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(enteredEmail)) {
+    errorMsg.style.opacity = "0";
+  } else {
+    errorMsg.style.opacity = "1";
+  }
+})
